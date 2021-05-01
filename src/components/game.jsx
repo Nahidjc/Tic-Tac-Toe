cf4ad74f616cc9c7341904a430a7634059f3c8b9
Nahid2194 <nahidjc80@gmail.com>
2021-05-01 14:36:47 +0600

    square and game component setup

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import Board from "./board";
 
 class game extends Component {
+  onClick = (i) => {
+    console.log(i);
+  };
+
+  state = {
+    history: [{ squares: Array(9).fill(null) }],
+  };
   render() {
     return (
       <div>
         <h1>Game Component</h1>
+        <Board onClick={this.onClick} squares={this.state.history[0]} />
       </div>
     );
   }
