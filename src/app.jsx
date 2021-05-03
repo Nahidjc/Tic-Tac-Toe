a16c863260a5af8cd51c40da19ab3178debcd397
Nahid2194 <nahidjc80@gmail.com>
2021-05-03 15:22:03 +0600

    updated

--- src/app.jsx
+++ src/app.jsx
@@ -4,8 +4,11 @@ import Game from "./components/game";
 const app = () => {
   return (
     <div className="App">
-      <h1>Tic Tac Toe</h1>
+      <h1>Tic Tac Toe Game</h1>
       <Game></Game>
+      <br />
+      <br />
+      <h3>Rimi Vs Nadim</h3>
     </div>
   );
 };
